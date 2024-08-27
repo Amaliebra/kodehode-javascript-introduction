c7fc4766cc897a9a96339a6708ca6b5baf4ed6a8
Amaliebra <jba082024@gmail.com>
2024-08-27 08:59:06 +0200

    oppgave4

--- main.js
+++ main.js
@@ -78,8 +78,18 @@ The function should return:
 
 ******************************************************************************/
 
-export const greeter = () => {
-  //your code here
+export const greeter = (name, hour) => {
+  if (hour < 0 || hour > 23) {
+    return "Invalid time";
+  } else if (hour >= 0 && hour <= 5) {
+    return "Good night " + name;
+  } else if (hour >= 6 && hour <= 11) {
+    return "Good morning " + name;
+  } else if (hour >= 12 && hour <= 17) {
+    return "Good day " + name;
+  } else if (hour >= 18 && hour <= 23) {
+    return "Good evening " + name;
+  }
 };
 
 /******************************************************************************
